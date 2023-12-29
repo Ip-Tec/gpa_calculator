b20cc665ac28680b401bba8599c653f63226a151
innocent peter <otakhorpeter@gmail.com>
2023-12-29 11:56:36 +0100

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,33 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'GPA Calculator',
-  description: 'GPA Calculator for Nigeria Uni',
-}
+  title: "GPA Calculator",
+  description: "GPA Calculator for Nigeria Uni",
+  twitter: {
+    title: "GPA Calculator",
+    description: "GPA Calculator for Nigeria Uni",
+    images: "/GPA_Calculator.png",
+  },
+  openGraph: {
+    title: "GPA Calculator",
+    description: "My Website Description",
+    siteName: "GPA Calculator",
+    images: "/GPA_Calculator.png",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
